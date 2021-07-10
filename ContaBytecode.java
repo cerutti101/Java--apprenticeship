ae2a192300a71fe4918d65b4993b15645850ce60
Kaio Cerutti <cerutti321@gmail.com>
2021-07-10 16:18:40 -0300

    Segunda versão ByteBanc
    Bytebanck-composto
    (feito a mão, mesmo tendo o código semelhante liberado pela plataforma)

--- ContaBytecode.java
+++ ContaBytecode.java
@@ -1,17 +1,17 @@
 
 public class ContaBytecode {
 
-		double saldo;
-		int agencia;
-		int numero;
-		String titular;
+	private double saldo;
+	int agencia;
+	int numero;
+	 Cliente titular;
 
 	public void deposita(double valor) {
-			this.saldo = this.saldo+valor;
-		}
-	
+		this.saldo = this.saldo + valor;
+	}
+
 	public boolean saca(double valor) {
-		if(this.saldo >=valor) {
+		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			return true;
 		} else {
@@ -19,17 +19,18 @@ public class ContaBytecode {
 			return false;
 		}
 	}
-	
+
 	public boolean transfere(double valor, ContaBytecode destino) {
-		if(this.saldo>=valor) {
+		if (this.saldo >= valor) {
 			saca(valor);
 			destino.deposita(valor);
 			return true;
-		}else {
+		} else {
 			System.out.println(" Não ha saldo suficiente!");
 			return false;
 		}
 	}
-	
-	
+	public double getSaldo() {
+		return this.saldo;
+	}
 }
